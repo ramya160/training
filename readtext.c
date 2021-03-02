d3432b899dfff0adc8af4e0798b4627982c2914a
ramya160 <79127668+ramya160@users.noreply.github.com>
2021-03-02 12:27:53 +0530

    Update readtext.c

--- readtext.c
+++ readtext.c
@@ -6,19 +6,19 @@ struct employee
     int empid;
     char empname[20];
 }obj1[10];
-struct employee obj2[10];
+//struct employee obj2[10];
 int main()
 {   int i;
     FILE *fp;
     fp=fopen("employeedb","rb");
     for(i=0;i<2;i++)
     {
-    fread(&obj2[i],sizeof(obj2),1,fp);
+    fread(&obj1[i],sizeof(obj1),1,fp);
     }
     for(i=0;i<2;i++)
     {
-        printf("%d %s",obj2[i].empid,obj2[i].empname);
+        printf("%d %s",obj1[i].empid,obj1[i].empname);
     }
-    printf("read successfully");
+    fclose(fp);
 }
 
