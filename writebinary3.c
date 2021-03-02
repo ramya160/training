5bdc4cb52694e2d5b88fc777479b0ddf326ae5be
ramya160 <79127668+ramya160@users.noreply.github.com>
2021-03-02 12:10:44 +0530

    Create writebinary3.c

---
+++ writebinary3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+struct employee
+{
+    int empid;
+    char empname[20];
+}obj1[10];
+int main()
+{   int i;
+    FILE *fp;
+    fp=fopen("employeedb","wb");
+    for(i=0;i<2;i++)
+    {
+        scanf("%d %s",&obj1[i].empid,&obj1[i].empname);
+    }
+    fwrite(&obj1[i],sizeof(obj1),1,fp);
+    printf("written successfully");
+}
