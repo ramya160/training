d8f3b5e34be22819eaadef287a9a839fcb56639d
ramya160 <79127668+ramya160@users.noreply.github.com>
2021-03-02 12:11:21 +0530

    Create readtext.c

---
+++ readtext.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+struct employee
+{
+    int empid;
+    char empname[20];
+}obj1[10];
+struct employee obj2[10];
+int main()
+{   int i;
+    FILE *fp;
+    fp=fopen("employeedb","r");
+    fread(&obj2[i],sizeof(obj2),1,fp);
+    for(i=0;i<2;i++)
+    {
+        printf("%d %s",obj1[i].empid,obj1[i].empname);
+    }
+    printf("read successfully");
+}
+
