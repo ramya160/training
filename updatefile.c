3d4518c7172ad78a8e92d8d227aef869126c9120
ramya160 <79127668+ramya160@users.noreply.github.com>
2021-03-02 12:43:07 +0530

    Create updatefile.c

---
+++ updatefile.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+struct employee
+{
+    int empid;
+    char empname[20];
+}obj1[10];
+int main()
+{   int i;int id;
+    FILE *fp;
+    fp=fopen("employeedb","rb");
+    printf("enter the id to update");
+    scanf("%d",&id);
+    while(fread(&obj1[i],sizeof(obj1),1,fp)==1)
+    {
+        if(obj1[i].empid==id)
+        {    
+            printf("enter new data");
+            scanf("%d %s",&obj1[i].empid,&obj1[i].empname);
+           fseek(fp,sizeof(obj1[i])*i,SEEK_SET);
+           fread(&obj1[i],sizeof(obj1),1,fp);
+         }
+    }
+}
+
