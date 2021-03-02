b57c589779d710d5665dac667e04c645290948ea
ramya160 <79127668+ramya160@users.noreply.github.com>
2021-03-02 14:00:38 +0530

    Update pipefork.c

--- pipefork.c
+++ pipefork.c
@@ -8,10 +8,7 @@ int main()
 
 {
 
-int pid=0;
-
-pid = fork();
- 
+int pid;
 int fds[2];
 int fds1[2];
 int data;
@@ -24,28 +21,25 @@ int data1;
       {
    printf("pipe error");
       } 
-   if(pid<0)
+   if((pid = fork())<0)
       {
    printf("error");
       }
           
 
-if(pid == 0){
+if(pid == 0)
+{
 
-printf("In Child");
   sleep(5);
   read(fds[0],data,5);
   write(fds1[1],"hai",5);
 }
 
 if(pid>0)
-{     printf("In Parent process");
+{
       write(fds[1],"hello",5);
        sleep(5);
        read(fds1[0],data1,5);
 }
-
-
-return 0;
 
 }
